55c6ac1ab7b057e538412b971ea1655541e8b5e4
Raffael N Ferreira <raffaelnascimentof@gmail.com>
2021-12-12 21:37:10 -0300

    Create post user

--- src/modules/users/useCases/createUser/CreateUserController.ts
+++ src/modules/users/useCases/createUser/CreateUserController.ts
@@ -6,7 +6,17 @@ class CreateUserController {
   constructor(private createUserUseCase: CreateUserUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { name, email } = request.body;
+
+    const user = this.createUserUseCase.execute({ name, email });
+
+    const userResponse = {
+      name: user.name,
+      email: user.email,
+      admin: user.admin,
+    };
+
+    return response.status(201).json(userResponse);
   }
 }
 
